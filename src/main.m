f8b390b6190fb26b792f492335704456ed130a9f
YuxingZhang <zhangyx233@gmail.com>
2015-11-25 17:45:23 -0500

    Going for dinner

--- src/main.m
+++ src/main.m
@@ -1,9 +1,32 @@
 %% The main program for the fast sampling Gaussian mixture model
-%% 
-%% Input format: d×n matrix, where d is the dimension of the data, 
-%% n is the number of data points we have
+
+
+%% Input format: D×N matrix, where D is the dimension of the data, 
+%% N is the number of data points we have
+filename = 'InputDataSet.dat';
+X = csvread(filename);
+D = size(X, 1);
+N = size(X, 2);
 
 %% K is the number of classes (number of mixture components) which 
 %% is specified by the user.
 K = 10;
+
+%% random sample the class labels initially
+Y = randi([1, K], 1, N);
+
+%% T is the number of iterations we sample
+T = 100;
+
+for t = 1 : T
+    
+end
+
+
+
+
+
+
+
+
 
