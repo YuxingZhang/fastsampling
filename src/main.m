2fb35a0d6c31472d56ed9e9e2411a84bd1dd5025
YuxingZhang <zhangyx233@gmail.com>
2015-11-25 16:48:14 -0500

    Adding some more comments, and specifying K

--- src/main.m
+++ src/main.m
@@ -1,2 +1,9 @@
 %% The main program for the fast sampling Gaussian mixture model
 %% 
+%% Input format: d×n matrix, where d is the dimension of the data, 
+%% n is the number of data points we have
+
+%% K is the number of classes (number of mixture components) which 
+%% is specified by the user.
+K = 10;
+
